569aca80c3dfb39eec1c4da9330a7c4f4677b277
filipe.reis <filipe.reis@verity.com.br>
2020-04-08 03:26:21 -0300

    Add project files.

---
+++ C4M.Domain.Infra/Configurations/IMongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace C4M.Domain.Infra.Configurations
+{
+    public interface IMongoDbSettings
+    {
+        string DatabaseName { get; set; }
+        string ConnectionString { get; set; }
+    }
+}
---
+++ C4M.Domain.Infra/Configurations/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace C4M.Domain.Infra.Configurations
+{
+    public class MongoDbSettings : IMongoDbSettings
+    {
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
