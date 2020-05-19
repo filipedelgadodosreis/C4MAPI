4b1488bdc1929b09ed4a248e42427f9d0f74b6e9
Filipe Reis <filipedelgadodosreis@gmail.com>
2020-05-18 22:18:41 -0300

    Criação do controller device e suas estruturas

---
+++ C4M.Domain.Api/Controllers/DeviceController.cs
@@ -0,0 +1,21 @@
+﻿using C4M.Api.Infrastructure;
+using C4M.Domain.Entities;
+using C4M.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C4M.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiConventionType(typeof(DeviceApiConventions))]
+    public class DeviceController : ControllerBaseApi
+    {
+        private readonly IMongoRepository<Person> _peopleRepository;
+
+        public DeviceController(IMongoRepository<Person> peopleRepository)
+        {
+            _peopleRepository = peopleRepository;
+        }
+
+
+    }
+}
