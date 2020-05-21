d6fb652b3bb8fe63bc63a84509ca212a87d9f7ab
Filipe Reis <filipedelgadodosreis@gmail.com>
2020-05-21 03:54:38 -0300

    Desenvolvimento automapper e dto

---
+++ C4M.Domain.Api/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using C4M.Api.ViewModel;
+
+namespace C4M.Api.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Domain.Entities.Device, DeviceCreate>();
+            CreateMap<Domain.Entities.Instrument, Instrument>();
+            CreateMap<Domain.Entities.Applicationinventory, Applicationinventory>();
+            CreateMap<Domain.Entities.Applicationpackage, Applicationpackage>();
+            CreateMap<Devicedate, Domain.Entities.Devicedate>();
+            CreateMap<Batterymonitorsettings, Domain.Entities.Batterymonitorsettings>();
+            CreateMap<Blacklist, Domain.Entities.Blacklist>();
+            CreateMap<Datausage, Domain.Entities.Datausage>();
+            CreateMap<Devicedate1, Domain.Entities.Devicedate1>();
+            CreateMap<Datausagemonitorsettings, Domain.Entities.Datausagemonitorsettings>();
+            CreateMap<Deviceinformation, Domain.Entities.Deviceinformation>();
+            CreateMap<Devicedate2, Domain.Entities.Devicedate2>();
+            CreateMap<Group, Domain.Entities.Group>();
+            CreateMap<Lastbatterydata, Domain.Entities.Lastbatterydata>();
+            CreateMap<Devicedate3, Domain.Entities.Devicedate3>();
+            CreateMap<Lastlocation, Domain.Entities.Lastlocation>();
+            CreateMap<Coordinate, Domain.Entities.Coordinate>();
+            CreateMap<Devicedate4, Domain.Entities.Devicedate4>();
+            CreateMap<Lastmemorydata, Domain.Entities.Lastmemorydata>();
+            CreateMap<Devicedate5, Domain.Entities.Devicedate5>();
+            CreateMap<Locationhistorymonitorsettings, Domain.Entities.Locationhistorymonitorsettings>();
+            CreateMap<Locationmonitorsettings, Domain.Entities.Locationmonitorsettings>();
+            CreateMap<Memorymonitorsettings, Domain.Entities.Memorymonitorsettings>();
+            CreateMap<Applicationusagestatisticsmonitorsettings, Domain.Entities.Applicationusagestatisticsmonitorsettings>();
+        }
+    }
+}
---
+++ C4M.Domain.Api/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using C4M.Api.ViewModel;
+
+namespace C4M.Api.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<DeviceCreate, Domain.Entities.Device>();
+            CreateMap<Instrument, Domain.Entities.Instrument>();
+            CreateMap<Applicationinventory, Domain.Entities.Applicationinventory>();
+            CreateMap<Applicationpackage, Domain.Entities.Applicationpackage>();
+            CreateMap<Devicedate, Domain.Entities.Devicedate>();
+            CreateMap<Batterymonitorsettings, Domain.Entities.Batterymonitorsettings>();
+            CreateMap<Blacklist, Domain.Entities.Blacklist>();
+            CreateMap<Datausage, Domain.Entities.Datausage>();
+            CreateMap<Devicedate1, Domain.Entities.Devicedate1>();
+            CreateMap<Datausagemonitorsettings, Domain.Entities.Datausagemonitorsettings>();
+            CreateMap<Deviceinformation, Domain.Entities.Deviceinformation>();
+            CreateMap<Devicedate2, Domain.Entities.Devicedate2>();
+            CreateMap<Group, Domain.Entities.Group>();
+            CreateMap<Lastbatterydata, Domain.Entities.Lastbatterydata>();
+            CreateMap<Devicedate3, Domain.Entities.Devicedate3>();
+            CreateMap<Lastlocation, Domain.Entities.Lastlocation>();
+            CreateMap<Coordinate, Domain.Entities.Coordinate>();
+            CreateMap<Devicedate4, Domain.Entities.Devicedate4>();
+            CreateMap<Lastmemorydata, Domain.Entities.Lastmemorydata>();
+            CreateMap<Devicedate5, Domain.Entities.Devicedate5>();
+            CreateMap<Locationhistorymonitorsettings, Domain.Entities.Locationhistorymonitorsettings>();
+            CreateMap<Locationmonitorsettings, Domain.Entities.Locationmonitorsettings>();
+            CreateMap<Memorymonitorsettings, Domain.Entities.Memorymonitorsettings>();
+            CreateMap<Applicationusagestatisticsmonitorsettings, Domain.Entities.Applicationusagestatisticsmonitorsettings>();
+        }
+    }
+}
